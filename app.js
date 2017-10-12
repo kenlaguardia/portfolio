d96cef1927e55db1b9b52030723f3033340b343b
Ken Laguardia <kenlaguardia2017@gmail.com>
2017-10-13 04:29:29 +0800

    add app.listen c9 code

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen("3000", function () {
+// app.listen("3000", function () {
+app.listen(process.env.PORT, process.env.IP, function () {
 	console.log("Server is Running");
 });
